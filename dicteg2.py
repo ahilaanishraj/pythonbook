3d98ec99345069a5e0a4b543ff6c8e9729ad2a1b
ahilaanishraj <65823666+ahilaanishraj@users.noreply.github.com>
2023-10-09 11:53:53 +0530

    Add files via upload

---
+++ dicteg2.py
@@ -0,0 +1,16 @@
+# Creating a dictionary
+my_dict = {
+    'name': 'Anu',
+    'age': 30,
+    'city': 'Nagarcoil'
+}
+
+# Accessing values using keys
+name = my_dict['name']  # Access the 'name' key's value ('Anu')
+age = my_dict['age']    # Access the 'age' key's value (30)
+city = my_dict['city']  # Access the 'city' key's value ('Nagarcoil')
+
+# Printing the values
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
